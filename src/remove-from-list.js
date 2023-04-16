95711336518fc73677c57a676e87fad809d0d936
Fiodar Siyanka <siyankafiodar@gmail.com>
2023-04-16 18:27:20 +0300

    feat: js files

--- src/remove-from-list.js
+++ src/remove-from-list.js
@@ -25,12 +25,6 @@ const { NotImplementedError } = require("../extensions/index.js");
 function removeKFromList(l, k) {
   // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
-
-  // if (l.value === k) {
-  //   l.value = null;
-  //   l = l.next;
-  // }
-  let prev = null;
   while (l.next) {
     if (l.value === k) {
       l.value = null;
